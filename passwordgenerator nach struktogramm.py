20b920aec67824ebac04a606816c495166b4a4c1
Caevee <pister.alex.cv@gmail.com>
2022-04-08 11:41:30 +0200

    Kommentare

--- passwordgenerator nach struktogramm.py
+++ passwordgenerator nach struktogramm.py
@@ -1,11 +1,16 @@
 import random
 
+# Variablen initialisieren
 laengePw = 0
 pw = ""
 
+# Loop durch die Passwortlänge
 while laengePw < 10:
+    # Random Zahl generieren
     random_num = random.randint(0,127)
-    if random_num > 32:
+    # Generierte Zahl abprüfen ob zwischen 32 und 127
+    if random_num > 32 and random_num < 127:
+        # if true hänge ASCII Zeichen an Stelle der generierten Zahl an das Passwort
         zeichen = chr(random_num)
         pw += zeichen
         laengePw += 1
